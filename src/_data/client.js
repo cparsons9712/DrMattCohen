cd18c3295e654d004d883f2d08073aa265ff0dc6
christine parsons <clong9712@gmail.com>
2024-11-15 11:10:07 -0500

    Add business data

--- src/_data/client.js
+++ src/_data/client.js
@@ -1,16 +1,16 @@
 module.exports = {
-    "name": "Code Stitch Web Designs",
-    "email": "help@codestitch.app",
-    "phoneForTel": "555-779-4407",
-    "phoneFormatted": "(555) 779-4407",
+    "name": "Dr.Matthew Cohen",
+    "email": "drmatthewcohen@yahoo.com",
+    "phoneForTel": "248-542-3653",
+    "phoneFormatted": "(248) 542-3653",
     "address": {
-        "lineOne": "First Address Line",
-        "lineTwo": "Second Address Line",
-        "city": "Denver",
-        "state": "CO",
-        "zip": "80206",
+        "lineOne": "Madison Heights Chiropractic Center",
+        "lineTwo": "28107 John R ",
+        "city": "Madison Heights",
+        "state": "MI",
+        "zip": "48071",
         "country": "US",
-        "mapLink": "https://goo.gl/maps/UAQn4vuGDiwv7DV39"
+        "mapLink": "https://www.google.com/maps/place/Madison+Heights+Chiropractic+Center/@42.4997211,-83.1086359,17z/data=!3m1!4b1!4m6!3m5!1s0x8824cf89f62017a1:0xb3ee26a53b91f2d6!8m2!3d42.4997211!4d-83.106061!16s%2Fg%2F1wc31wd0?entry=ttu&g_ep=EgoyMDI0MTExMi4wIKXMDSoASAFQAw%3D%3D"
     },
     "socials": {
         "facebook": "https://www.facebook.com/",
